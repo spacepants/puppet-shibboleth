431f9e664c5a44e07152ae9a2a4ff78451057863
Paul Bailey <paulba@stedwards.edu>
2016-02-04 13:04:35 -0600

    typo

--- manifests/webapp.pp
+++ manifests/webapp.pp
@@ -1,11 +1,11 @@
 # == Class shibboleth::webapp
 #
 # This class is meant to be called from shibboleth.
-# It builds the idm.war webapp.
+# It builds the idp.war webapp.
 #
 class shibboleth::webapp {
 
-  exec { 'build idm.war':
+  exec { 'build idp.war':
     command => "${shibboleth::idp_home}/bin/build.sh",
     # path => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     environment => "JAVA_HOME=${shibboleth::java_home}",
