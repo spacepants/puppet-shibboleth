58f604aa35feac2b2af8fe4ba2566828fd4480dc
Paul Bailey <paulba@stedwards.edu>
2016-02-03 23:17:51 -0600

    add merge properties file

--- manifests/install.pp
+++ manifests/install.pp
@@ -9,7 +9,7 @@ class shibboleth::install {
     staging_path => "/opt/staging/shibboleth-identity-provider-${shibboleth::version}.tar.gz",
     creates => "/opt/staging/shibboleth-identity-provider-${shibboleth::version}",
   }->
-  file { '/opt/staging/shibboleth-identity-provider-${shibboleth::version}/conf/shibboleth.properties':
+  file { "/opt/staging/shibboleth-identity-provider-${shibboleth::version}/conf/shibboleth.properties":
     ensure  => file,
     content => template('shibboleth/shibboleth.properties.erb'),
     notify  => Exec['bootstrap idp home'],
