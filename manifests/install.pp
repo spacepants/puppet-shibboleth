1d189872e00a289da8e95cd70810855846ecf891
Paul Bailey <paulba@stedwards.edu>
2016-02-04 08:25:36 -0600

    fix erp->puppet vars

--- manifests/install.pp
+++ manifests/install.pp
@@ -18,14 +18,14 @@ class shibboleth::install {
     # path => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     environment => [
       "ANT_OPTS=\"
-        -Didp.src.dir=/opt/staging/shibboleth-identity-provider-<%= @version %>
-        -Didp.target.dir=<%= @idp_home %>
-        -Didp.host.name=<%= @host_name %>
+        -Didp.src.dir=/opt/staging/shibboleth-identity-provider-${shibboleth::version}
+        -Didp.target.dir=${shibboleth::idp_home}
+        -Didp.host.name=${shibboleth::host_name}
         -Didp.noprompt=yes
         -Didp.no.tidy=yes
-        -Didp.keystore.password=<%= @keystore_pass %>
-        -Didp.sealer.password=<%= @sealer_pass %>
-        -Didp.merge.properties=/opt/staging/shibboleth-identity-provider-<%= @version %>/shibboleth.properties \"",
+        -Didp.keystore.password=${shibboleth::keystore_pass}
+        -Didp.sealer.password=${shibboleth::sealer_pass}
+        -Didp.merge.properties=/opt/staging/shibboleth-identity-provider-${shibboleth::version}/shibboleth.properties \"",
       "JAVA_HOME=${shibboleth::java_home}",
     ],
     refreshonly => true,
